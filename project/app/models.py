774b1af10235c5ecebee271462772ef621cd792f
Yrka <varnack30@gmail.com>
2023-06-27 21:28:54 +0300

    + __str__ in class Quote

--- project/app/models.py
+++ project/app/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Quote(models.Model):
     quote = models.TextField(blank=False)
     author = models.CharField(max_length=20, blank=False)
+
+    def __str__(self):
+        return str(self.id)
