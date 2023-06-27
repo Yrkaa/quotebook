081f9291e09a2e73fa5f0a01d515c2cf46d266cc
Yrka <varnack30@gmail.com>
2023-06-27 21:36:58 +0300

    + symbols in author in class Quote

--- project/app/models.py
+++ project/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Quote(models.Model):
     quote = models.TextField(blank=False)
-    author = models.CharField(max_length=20, blank=False)
+    author = models.CharField(max_length=40, blank=False)
 
     def __str__(self):
         return str(self.id)
