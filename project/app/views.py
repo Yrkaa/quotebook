b8d4857a96993938374cd9c82d042fde425f6e8d
Yrka <varnack30@gmail.com>
2023-06-27 21:40:48 +0300

    update main page view

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from app.models import Quote
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    quotes = Quote.objects.all()
+    return render(request, 'index.html', context={'quotes': quotes})
