696bd483aa69048c1c438dc147516bc7222085de
Yrka <varnack30@gmail.com>
2023-06-27 21:26:01 +0300

    create db class Quote

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from app.models import Quote
+
 # Register your models here.
+admin.site.register(Quote)
--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Quote(models.Model):
+    quote = models.TextField(blank=False)
+    author = models.CharField(max_length=20, blank=False)
