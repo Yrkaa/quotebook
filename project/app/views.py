bc3264b23b78142c7a55bda6a168f415bfa11adc
Yrka <varnack30@gmail.com>
2023-06-28 21:37:00 +0300

    update create page view

--- project/app/views.py
+++ project/app/views.py
@@ -8,4 +8,9 @@ def index(request):
     return render(request, 'index.html', context={'quotes': quotes})
 
 def create(request):
+    if request.method == 'POST':
+        quote = request.POST.get('quote')
+        author = request.POST.get('author')
+        new_quote = Quote(quote=quote, author=author)
+        new_quote.save()
     return render(request, 'create_quote.html')
